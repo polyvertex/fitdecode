9fd2e4d36fb7afcb1fa6be881f2c7cd2443e2b12
Jean-Charles Lefebvre <polyvertex@gmail.com>
2018-07-08 23:16:24 +0200

    correction

--- clean.cmd
+++ clean.cmd
@@ -8,6 +8,7 @@ rmdir /s /q dist 2>nul
 rmdir /s /q fitdecode.egg-info 2>nul
 
 rmdir /s /q fitdecode\__pycache__ 2>nul
+rmdir /s /q fitdecode\cmd\__pycache__ 2>nul
 
 rmdir /s /q tests\__pycache__ 2>nul
 
